314b376d6072f49ed661dca710ffeb65174e27eb
Fathimathul Faiza <fathimthulfaiza@gmail.com>
2026-01-26 09:01:34 +0530

    Add User and Product models with mongoose schemas

---
+++ src/models/productModel.js
@@ -0,0 +1,42 @@
+
+import mongoose from "mongoose"
+
+const productSchema = new mongoose.Schema({
+    title : {
+        type : String,
+        required : true
+    },
+    author : {
+        type : String,
+        required : true
+    },
+    description : {
+        type : String,
+        required : String
+    },
+    price : {
+        type : Number,
+        required : true
+    },
+    stock : {
+        type : Number,
+        required : true,
+        default : 0
+    },
+    images : {
+        type : [String],
+        required : true
+    },
+    category : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "Category",
+        required : true
+    }
+
+},{timestamps : true}
+)
+
+
+const Product = mongoose.model("Product", productSchema)
+
+export default Product
