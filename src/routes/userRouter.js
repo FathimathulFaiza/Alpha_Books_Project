d948db2aca9b63e7dd80b9a963bd87011c1057a9
Fathimathul Faiza <fathimthulfaiza@gmail.com>
2026-01-28 12:17:12 +0530

    "feat: shop page search, sort, category filter with active products"

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -32,29 +32,72 @@ router.get("/change-password", (req, res) => {
   res.render("user/change_password");
 });
 
-// Shop
+
+
+// Shop page
+
 router.get("/shop", async (req, res) => {
 
+  // Quer params
   const selectedCategory = req.query.category
 
+  const search = req.query.search || ""       // search query
+
+  const sort = req.query.sort || ""
+
   console.log("ID of the selected category is : ", selectedCategory)
+   console.log("Search:", search);
 
-  const categories = await Category.find({isActive : true})
+// pagination
 
-  const products = selectedCategory
-  ? await Product.find(
-    {category : selectedCategory,
-     isActive : true}) 
-  : []
+
+
+  const categories = await Category.find({isActive : true})     // show only the category which is active - true
+
+  // sorting 
+  let sortQuery = {}
+
+  if(sort === "price_asc"){    // ascending price  (low → high) - price
+    sortQuery.price = 1
+  }
+
+  else if(sort === "price_desc"){   
+    sortQuery.price = -1
+
+  }
+  else if(sort === "name_asc"){   // string sort alphabetically ->  A → Z  - title
+    sortQuery.title = 1
+  }
+  else if(sort === "name_desc"){
+    sortQuery.title = -1
+  }
+
+
+  let products = []
+
+  if(selectedCategory){               // Shows products only from selected category
+
+    products = await Product.find({
+      category : selectedCategory,
+      isActive : true,    // hides the blocked,unlisted, soft deleted products - shows only active products
+      $or:[
+        {title : {$regex : search, $options : "i"}},    // filter search by title and author   -> "i" = case-insensitive
+        {author : {$regex : search, $options : "i"}}
+      ]
+    }).sort(sortQuery)    //  applies whichever sorting user selected:
+  }
 
   console.log("Products Count : ",products.length)
 
-  res.render("user/shop",{
+  res.render("user/shop",{       // rendering all these
     categories,
     selectedCategory,
-    products
+    products,
+    search
   });
 });
+
+
 
 router.get("/product/:id", async(req, res) => {
   const productId = req.params.id
